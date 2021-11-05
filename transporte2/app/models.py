143dbbb300265cee6a25f149462723877592ec77
Diego Ortiz <diego@innova3x.com>
2021-11-05 00:53:29 -0300

    nuevos

--- transporte2/app/models.py
+++ transporte2/app/models.py
@@ -1,12 +1,190 @@
+import datetime
+from typing import Text
+from . import appbuilder, db
 from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Date, Table
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm.session import SessionTransaction
+from sqlalchemy.sql.sqltypes import Numeric
+from flask import session,request
+from wtforms import Form, StringField
+from wtforms.validators import DataRequired
+from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
+from flask_appbuilder.forms import DynamicForm
+from flask_appbuilder.api import BaseApi, expose, rison ,safe
+from . import appbuilder
+import prison
+schema = {
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string"
+        }
+    }
+}
 
-"""
+print(prison.dumps(schema))
 
-You can use the extra Flask-AppBuilder fields and Mixin's
+class ExampleApi(BaseApi):
+    #route_base = '/newapi/v2/nice'
+    resource_name = 'example'
+    @expose('/greeting4')
+    @rison(schema)
+    def greeting4(self, **kwargs):
+        return self.response(
+        200,
+        message=f"Hello {kwargs['rison']['name']}"
+    )
+    @expose('/error')
+    @safe
+    def error(self):
+       raise Exception
 
-AuditMixin will add automatic timestamp of created and modified by who
+appbuilder.add_api(ExampleApi)
 
 
-"""
+
+class MyForm(DynamicForm):
+    celular = StringField(('Celular'),
+        description=('Ingresar numero de celular!'),
+        validators = [DataRequired()], widget=BS3TextFieldWidget())
+    mensaje = StringField(('Mensaje'),
+        description=('Mensaje!'), widget=BS3TextFieldWidget())
+
+
+   
+cli = []
+
+
+def today():
+    return datetime.datetime.today().strftime('%d/%m/%Y')
+
+
+class Saldos(Model):
+
+    sid = Column(Integer, primary_key=True)
+    fecha = Column(String, default=today, nullable=True)
+    cliente_id = Column(Integer, ForeignKey("clientes.id"), nullable=False)
+    Clientes = relationship("Clientes")
+    saldo = Column(Float)
+
+
+class DetalleServicioTecnico(Model):
+
+    dstid = Column(Integer, primary_key=True)
+    detalle = Column(String)
+    costoManoDeObra = Column(Float)
+    repuesto_id = Column(Integer, ForeignKey("repuestos.id"), nullable=False)
+    repuestos = relationship("Repuestos")
+    servicioTecnico_id = Column(Integer, ForeignKey(
+        "servicio_tecnico.stid"), nullable=False)
+    servicio_tecnico = relationship("ServicioTecnico")
+
+
+class Repuestos(Model):
+    id = Column(Integer, primary_key=True)
+    categoria = Column(String)
+    descripcion = Column(String)
+    precio = Column(Float)
+
+    def __repr__(self):
+        return f"('{self.descripcion}', '{self.precio}')"
+
+
+
+class ServicioTecnico(Model):
+    
+    stid = Column(Integer, primary_key=True)
+    fecha = Column(String, default=today, nullable=True)
+    costo = Column(Float)
+    formaPago = Column(Integer, ForeignKey(
+        "forma_de_pago.fdpid"), nullable=False)
+    forma_de_pago = relationship("formaDePago")
+    cliente_id = Column(Integer, ForeignKey("clientes.id"), nullable=False)
+    Clientes = relationship("Clientes")
+    camion_id = Column(Integer, ForeignKey("camiones.cid"), nullable=False)
+    camiones = relationship("Camiones")
+
+    def __repr__(self):
+
+        return f"('{self.costo}', '{self.cliente_id}', '{self.fecha}')"
+
+
+class formaDePago(Model):
+
+    fdpid = Column(Integer, primary_key=True)
+    tipoDePago = Column(String(75), nullable=False)
+
+    def __repr__(self):
+        return f"('{self.tipoDePago}')"
+        
+class ClienteServicioTecnico(Model):
+    cstid = Column(Integer, primary_key=True)
+    cliente_id = Column(Integer, ForeignKey("clientes.id"), nullable=False)
+    Clientes = relationship("Clientes")
+    servicioTecnico_id = Column(Integer, ForeignKey(
+        "servicio_tecnico.stid"), nullable=False)
+    servicio_tecnico = relationship("ServicioTecnico")
+    fecha = Column(String, default=today, nullable=True)
+    def __repr__(self):
+        return f"('{self. servicioTecnico_id}')"
+
+class ClienteCamiones(Model):
+    id = Column(Integer, primary_key=True)
+    cliente_id = Column(Integer, ForeignKey("clientes.id"), nullable=False)
+    Clientes = relationship("Clientes")
+    camion_id = Column(Integer, ForeignKey("camiones.cid"), nullable=False)
+    camiones = relationship("Camiones")
+    fecha = Column(String, default=today, nullable=True)
+    def __repr__(self):
+        return f"('{self.camion_id}')"
+
+class Camiones(Model):
+
+    cid = Column(Integer, primary_key=True)
+    patente = Column(String(75), nullable=False)
+    marca = Column(String(75), nullable=False)
+    modelo = Column(String(75), nullable=False)
+    cliente_id = Column(Integer, ForeignKey("clientes.id"), nullable=False)
+    clientes = relationship("Clientes")
+
+    def __repr__(self):
+        return f"('{self.marca}', '{self.modelo}')"
+
+
+class Localidad(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+
+assoc_localidades_clientes = Table(
+    "localidades_clientes",
+    Model.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("localidad_id", Integer, ForeignKey("localidad.id")),
+    Column("cliente_id", Integer, ForeignKey("clientes.id")),
+)
+
+
+class Clientes(Model):
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(75), nullable=False)
+    apellido = Column(String(75), nullable=False)
+    telefono = Column(String(25), nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    direccion = Column(String(120), unique=True, nullable=False)
+    localidad = relationship(
+        "Localidad", secondary=assoc_localidades_clientes, backref="cliente"
+    )
+   
+    def __repr__(self):
+    
+        return f"('{self.nombre}', '{self.apellido}')"
+
+
+
+
--- transporte2/app/views.py
+++ transporte2/app/views.py
@@ -1,8 +1,148 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,session
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder import ModelView, ModelRestApi
-
+from sqlalchemy.orm.session import SessionTransaction
+from app import app
 from . import appbuilder, db
+from app import app
+from .models import  ServicioTecnico,Camiones,Clientes,Saldos,Localidad,Repuestos,DetalleServicioTecnico, formaDePago,ClienteCamiones,ClienteServicioTecnico,MyForm
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from flask_appbuilder.fieldwidgets import Select2Widget
+from flask_appbuilder.actions import action
+import pyautogui as pg
+import webbrowser as web
+import time
+
+def camiones_query():
+    return db.session.query(Camiones)
+
+def servicio_tecnico_query():
+    return db.session.query(ServicioTecnico)   
+
+   
+
+class ClienteServicioTecnicoView(ModelView):
+    datamodel = SQLAInterface(ClienteServicioTecnico)
+    list_columns = [ "servicioTecnico_id"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+    
+    
+
+class ClienteCamionesView(ModelView):
+    datamodel = SQLAInterface(ClienteCamiones)
+    list_columns = [ "camion_id"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+    
+
+class formaDePagoView(ModelView):
+    datamodel = SQLAInterface(formaDePago)
+    list_columns = ["tipoDePago"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+    
+
+class SaldosView(ModelView):
+    datamodel = SQLAInterface(Saldos)
+    list_columns = ["fecha","cliente_id","saldo"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+    
+
+class DetalleServicioTecnicoView(ModelView):
+    datamodel = SQLAInterface(DetalleServicioTecnico)
+    list_columns = ["detalle", "costoManoDeObra", "repuesto_id","servicoTecnico_id"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+      
+
+class ServicioTecnicoView(ModelView):
+    datamodel = SQLAInterface(ServicioTecnico) 
+    list_columns = ["fecha", "costo", "cliente_id","camion_id"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+
+class RepuestosView(ModelView):
+    datamodel = SQLAInterface(Repuestos)
+    list_columns = ["categoria","descripcion","precio"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+    
+    
+
+
+class CamionesView(ModelView):
+    datamodel = SQLAInterface(Camiones)
+    list_columns = ["patente","marca", "modelo", "cliente_id"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+       
+    
+class ClientesView(ModelView):
+    datamodel = SQLAInterface(Clientes)
+    list_columns = ["nombre", "apellido", "email"]
+
+    @action("muldelete", "Borrar todos los items", "Estas seguro de borrar?", "fa-rocket", single=False)
+    def muldelete(self, items):
+       self.datamodel.delete_all(items)
+       self.update_redirect()
+       return redirect(self.get_redirect())
+
+    
+   
+    edit_form_extra_fields = {
+       "Camiones": QuerySelectField(
+            "Camiones",
+            query_factory=camiones_query,
+            widget=Select2Widget(extra_classes="readonly"),
+        )
+    }
+    
+    edit_form_extra_fields = {
+       "servicio_tecnico": QuerySelectField(
+            "ServicioTecnico",
+            query_factory=servicio_tecnico_query,
+            widget=Select2Widget(extra_classes="readonly"),
+        )
+    }
+
+    related_views = [ClienteCamionesView,ClienteServicioTecnicoView]
+    
+    show_template = "appbuilder/general/model/show_cascade.html" 
+    
 
 """
     Create your Model based REST API::
@@ -37,6 +177,7 @@ from . import appbuilder, db
 """
 
 
+
 @appbuilder.app.errorhandler(404)
 def page_not_found(e):
     return (
@@ -47,4 +188,95 @@ def page_not_found(e):
     )
 
 
+
+
+
+class LocalidadView(ModelView):
+    datamodel = SQLAInterface(Localidad)
+    add_columns = ["name"]
+    edit_columns = ["name"]
+    show_columns = ["name"]
+    list_columns = ["name"]
+
+
+
+from flask import flash
+from flask_appbuilder import SimpleFormView
+from flask_babel import lazy_gettext as _
+
+
+class MyFormView(SimpleFormView):
+    form = MyForm
+    form_title = 'This is my first form view'
+    message = 'My form submitted'
+
+    def form_get(self, form):
+        form.celular.data = 'Celular'
+        form.mensaje.data = 'Mensaje'
+
+    def form_post(self, form):
+        celulares = form.celular.data
+        mensajes = form.mensaje.data
+        first = True
+        
+        time.sleep(8)
+        web.open_new_tab("https://web.whatsapp.com/send?phone="+str(celulares)+"&text="+str(mensajes))
+        if first:
+            time.sleep(8)
+            first=False
+        width,height = pg.size()
+        pg.click(width/2,height/2)
+        time.sleep(20)
+        pg.press('enter')
+        time.sleep(6)
+        pg.hotkey('ctrl', 'w')
+       
+        return redirect(self.get_redirect())
+        
+
+
+
+
 db.create_all()
+
+appbuilder.add_view(
+   ClientesView, "Clientes", icon="fa-folder-open-o", category="Clientes"
+)
+appbuilder.add_view(
+    SaldosView, "Saldos", icon="fa-folder-open-o", category= "Clientes"
+)
+
+#appbuilder.add_separator("Clientes")
+appbuilder.add_view(
+    CamionesView, "Camiones", icon="fa-folder-open-o", category="Camiones"
+)
+appbuilder.add_view(
+    ServicioTecnicoView, "ServicioTecnico", icon="fa-folder-open-o", category= "ServicioTecnico"
+
+)
+appbuilder.add_view(
+    DetalleServicioTecnicoView, "DetalleServicioTecnico", icon="fa-folder-open-o", category= "ServicioTecnico"
+
+)
+appbuilder.add_view(
+     RepuestosView, " Repuestos", icon="fa-folder-open-o", category= "Camiones"
+
+)
+
+appbuilder.add_view(
+    LocalidadView, "Localidad", icon="fa-folder-open-o", category="Clientes"
+)
+appbuilder.add_view(
+    formaDePagoView, "FormaDePago", icon="fa-folder-open-o", category= "ServicioTecnico"
+)
+
+appbuilder.add_view_no_menu(ClienteCamionesView,"ClienteCamionesView")
+
+appbuilder.add_view_no_menu(ClienteServicioTecnicoView,"ClienteServicioTecnicoView")
+
+
+
+appbuilder.add_link("Calculos", icon="fa-folder-open-o", href='https://www.onlinerekenmachine.com/calculadora', category='ServicioTecnico')
+
+appbuilder.add_view(MyFormView, "My form View", icon="fa-group", label=_('Enviar msj '),
+                     category="My Forms", category_icon="fa-cogs")   
