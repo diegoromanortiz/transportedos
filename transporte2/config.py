fe57f32f42195867553e0d444d0eda2106241ad3
Diego Ortiz <diego@innova3x.com>
2021-11-05 01:03:33 -0300

    Argentina

--- transporte2/config.py
+++ transporte2/config.py
@@ -77,6 +77,7 @@ LANGUAGES = {
     "zh": {"flag": "cn", "name": "Chinese"},
     "ru": {"flag": "ru", "name": "Russian"},
     "pl": {"flag": "pl", "name": "Polish"},
+    "ar": {"flag": "ar", "name": "Argentina"},
 }
 # ---------------------------------------------------
 # Image and file configuration
